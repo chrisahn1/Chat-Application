c799c09bdfc091b4938eae853bbc063bbe477372
Christopher Ahn <chrisahn90@protonmail.com>
2025-09-09 10:18:41 +0000

    Fixed client

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,9 @@
-//ROUTES
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return <div className="App">Hello world</div>;
+}
+
+export default App;
