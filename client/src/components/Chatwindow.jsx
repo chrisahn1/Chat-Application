dc047ae4a8c4d6b415dd99879aea7e4047954dcb
Christopher Ahn <chrisahn90@protonmail.com>
2025-09-19 23:11:02 +0000

    Authcontext set for chatwindow

--- client/src/components/Chatwindow.jsx
+++ client/src/components/Chatwindow.jsx
@@ -1,14 +1,32 @@
-import { useNavigate, useLocation, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../context/AuthContext';
 import axiosJWT from '../axiosFolder/AxiosFile';
 
 function Chatwindow({ socket }) {
   const navigate = useNavigate();
 
+  const {
+    currentUsername,
+    setCurrentUsername,
+    currentUserID,
+    setCurrentUserID,
+    accessToken,
+    setAccessToken,
+    setIsAuth,
+  } = useContext(AuthContext);
+
+  const [disableSend, setDisableSend] = useState(true);
+
   const logout = async () => {
     await axiosJWT.delete('/users/logout', {
       withCredentials: true,
     });
+    setIsAuth(false);
+    setAccessToken({});
+    setCurrentUsername('');
+    setCurrentUserID('');
+
     navigate('/', { replace: true });
   };
 
