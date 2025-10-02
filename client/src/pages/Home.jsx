a44474585eb34ee5759556fe8856f6e91aea4604
Christopher Ahn <chrisahn90@protonmail.com>
2025-10-02 14:02:01 +0000

    Initial commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import React, { useContext, useState } from 'react';
 import { AuthContext } from '../context/AuthContext';
 import LoginCheck from '../modals/ModalLoginCheck';
-
+//TESTING
 function Home() {
   const navigate = useNavigate();
 
